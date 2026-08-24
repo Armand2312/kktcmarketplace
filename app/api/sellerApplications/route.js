0a47e3ab65378676604d57692664eaf286dde3d8
Armand Oral <armand.oral231@gmail.com>
2026-08-24 19:19:14 +0300

    Added basic auth logic

--- app/api/sellerApplications/route.js
+++ app/api/sellerApplications/route.js
@@ -1,11 +1,11 @@
 export async function GET() {
     try {
-        const sellerApplication = await prisma.sellerApplication.findMany({
+        const sellerApplications = await prisma.sellerApplication.findMany({
             orderBy: {
                 createdAt: "desc",
             }
         });
-        return new Response(JSON.stringify(sellerApplication), { status: 200 })
+        return new Response(JSON.stringify(sellerApplications), { status: 200 })
     } catch (error) {
         console.error("Error fetching seller applications:", error);
         return new Response(JSON.stringify({ error: "Failed to fetch seller applications" }), { status: 500 });
